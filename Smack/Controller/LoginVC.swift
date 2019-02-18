c08180ff0a6ea053d9207a99123df372a233e3f7
Mrwan Mohsen <mrwanmohsen@Mrwans-Mac.local>
2019-02-18 22:48:14 +0200

    Designed Login View

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,22 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Mrwan Mohsen on 2/18/19.
+//  Copyright © 2019 Mrwan Mohsen. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constant.swift
+//  Smack
+//
+//  Created by Mrwan Mohsen on 2/18/19.
+//  Copyright © 2019 Mrwan Mohsen. All rights reserved.
+//
+
+import Foundation
+let TO_LOGIN = "toLogin"
