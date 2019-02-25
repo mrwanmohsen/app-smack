97af9937556dbb614d4107c1838a534b9ae99508
Mrwan Mohsen <mrwanmohsen@Mrwans-Mac.local>
2019-02-25 17:20:30 +0200

    registeration-createUser

--- Smack/Services/AuthService.swift
+++ Smack/Services/AuthService.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class AuthService {
     
@@ -22,12 +23,12 @@ class AuthService {
         }
     }
     
-    var authTokken: String {
+    var authToken: String {
         get {
-            return defaults.value(forKey: TOKKEN_KEY) as! String
+            return defaults.value(forKey: TOKEN_KEY) as! String
         }
         set {
-            defaults.set(newValue, forKey: TOKKEN_KEY)
+            defaults.set(newValue, forKey: TOKEN_KEY)
         }
     }
     
@@ -42,14 +43,13 @@ class AuthService {
     
     func registerUser(email: String, password: String, compelation: @escaping CompletionHandler) {
         let lowerCaseEmail = email.lowercased()
-        let header = [
-            "Content-Type" : "application/json; charset=utf-8"
-        ]
+       
         let body: [String: Any] = [
             "email": lowerCaseEmail,
             "password": password
         ]
-        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString
+        
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseString
             { (response) in
                 if response.result.error == nil {
                     compelation(true)
@@ -59,4 +59,81 @@ class AuthService {
                 }
         }
     }
+    
+    func loginUser(email: String, password: String, compelation: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        Alamofire.request(URL_LOGIN, method: .post, parameters: body, encoding: JSONEncoding.default, headers: HEADER).responseJSON {
+            (response) in
+                if response.result.error == nil {
+//                    if let json = response.result.value as? Dictionary<String, Any> {
+//                        if let email = json["user"] as? String {
+//                            self.userEmail = email
+//                       }
+//                        if let token = json["token"] as? String {
+//                            self.authToken = token
+//                        }
+//                   }
+                    
+                // using swiftyJSON
+                    guard let data = response.data else { return }
+                    do {
+                     let json = try JSON(data: data)
+                        self.userEmail = json["user"].stringValue
+                        self.authToken = json["token"].stringValue
+                    } catch {
+                        print(error)
+                    }
+                    self.isLoggedIn = true
+                    compelation(true)
+                }else{
+                    compelation(false)
+                    debugPrint(response.result.error as Any)
+                }
+        }
+    }
+    
+    func createUser(name: String, email: String, avatarName: String, avatarColor: String, compelation: @escaping CompletionHandler) {
+        let lowerCaseEmail = email.lowercased()
+        
+        let body: [String: Any] = [
+            "name": name,
+            "email": lowerCaseEmail,
+            "avatarName": avatarName,
+            "avatarColor": avatarColor
+        ]
+        
+        let header = [
+            "Authorization": "Authorization\(AuthService.instance.authToken)",
+            "Content-Type" : "application/json; charset=utf-8"
+        ]
+        Alamofire.request(URL_USER_ADD, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseJSON {
+            (response) in
+            if response.result.error == nil{
+                
+                // using swiftyJSON
+                guard let data = response.data else { return }
+                do {
+                    let json = try JSON(data: data)
+                    let id = json["_id"].stringValue
+                    let color = json["avatarColor"].stringValue
+                    let avatarName = json["avatarName"].stringValue
+                    let email = json["email"].stringValue
+                    let name = json["name"].stringValue
+                    
+                    UserDataService.instance.setUserData(id: id, color: color, avatarName: avatarName, email: email, name: name)
+                } catch {
+                    print(error)
+                }
+                compelation(true)
+            }else{
+                compelation(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }  
 }
---
+++ Smack/View/RoundedButton.swift
@@ -0,0 +1,33 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Mrwan Mohsen on 2/24/19.
+//  Copyright © 2019 Mrwan Mohsen. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+
+}
